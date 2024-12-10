2aba1ae81c6ac88eab21b5e0c5c45ab9d26a9a15
Valeriya Petkova <valeriya-petkova@abv.bg>
2024-12-10 22:48:44 +0200

    Add footer component

--- taskManagementProject/task-management/src/app/core/core.module.ts
+++ taskManagementProject/task-management/src/app/core/core.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from "@angular/common";
 import { RouterModule } from "@angular/router";
 import { HeaderComponent } from "./header/header.component";
 import { ErrorComponent } from "./error/error.component";
+import { FooterComponent } from "./footer/footer.component";
 
 
 @NgModule({
-    declarations: [HeaderComponent, ErrorComponent],
+    declarations: [HeaderComponent, FooterComponent, ErrorComponent],
     imports: [CommonModule, RouterModule],
-    exports: [HeaderComponent],
+    exports: [HeaderComponent, FooterComponent],
 })
 
 export class CoreModule {}
