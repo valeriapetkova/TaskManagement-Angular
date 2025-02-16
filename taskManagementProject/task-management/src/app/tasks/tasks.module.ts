eb0c3015885dfb94cec6195917cffd5eddf77f81
Valeriya Petkova <valeriya-petkova@abv.bg>
2025-02-16 18:49:10 +0200

    Add tasks details component

--- taskManagementProject/task-management/src/app/tasks/tasks.module.ts
+++ taskManagementProject/task-management/src/app/tasks/tasks.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { FormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { TasksListComponent } from "./tasks-list/tasks-list.component";
 import { CreateTaskComponent } from "./create-task/create-task.component";
+import { TasksDetailsComponent } from "./tasks-details/tasks-details.component";
+import { RouterModule } from "@angular/router";
+import { SharedModule } from "../shared/shared.module";
+import { TasksRoutingModule } from "./tasks-routing.module";
 
 @NgModule({
-    declarations: [TasksListComponent, CreateTaskComponent],
-    imports: [CommonModule, FormsModule],
+    declarations: [TasksListComponent, CreateTaskComponent, TasksDetailsComponent],
+    imports: [CommonModule, FormsModule, ReactiveFormsModule, RouterModule, SharedModule, TasksRoutingModule],
     exports: [TasksListComponent],
 })
 
